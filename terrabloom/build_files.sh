dc0776eece6d5ef1940cf2b6de5f264ea4864a64
Kennedy <kennedy@gmail.com>
2026-05-15 16:23:18 +0300

    Align environment python runtimes

--- terrabloom/build_files.sh
+++ terrabloom/build_files.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "==> Upgrading pip..."
-python3.9 -m pip install --upgrade pip
+python3.14 -m pip install --upgrade pip
 
 echo "==> Installing dependencies..."
-python3.9 -m pip install -r requirements.txt
+python3.14 -m pip install -r requirements.txt
 
 echo "==> Running Database Migrations..."
-python3.9 manage.py migrate --noinput
+python3.14 manage.py migrate --noinput
 
 echo "==> Collecting static assets..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.14 manage.py collectstatic --noinput --clear
 
 echo "==> Build process completed successfully!"
